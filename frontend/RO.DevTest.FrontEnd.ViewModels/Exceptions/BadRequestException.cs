3b5ee0bd43a67f185080a8bb4c22836837d7203b
Jefferson C. Silva <jefferson.c.silva@outlook.com>
2025-04-25 19:58:17 -0300

    🚧 criado funcionalidade de mostra modal na aplicação

---
+++ frontend/RO.DevTest.FrontEnd.ViewModels/Exceptions/BadRequestException.cs
@@ -0,0 +1,7 @@
+﻿using System.Net;
+
+namespace RO.DevTest.FrontEnd.ViewModels.Exceptions;
+public sealed class BadRequestException(HttpStatusCode statusCode, string message) : Exception(message)
+{
+	public HttpStatusCode StatusCode { get; init; } = statusCode;
+}
