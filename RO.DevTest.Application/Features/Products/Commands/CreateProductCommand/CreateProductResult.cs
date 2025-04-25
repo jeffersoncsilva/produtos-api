e88b70ea77e5b24a50b4053b90a3998c02ea743e
Jefferson C. Silva <jefferson.c.silva@outlook.com>
2025-04-25 10:31:59 -0300

    🚧adição de comandos e classes para a criação de um produto

---
+++ RO.DevTest.Application/Features/Products/Commands/CreateProductCommand/CreateProductResult.cs
@@ -0,0 +1,10 @@
+﻿namespace RO.DevTest.Application.Features.Products.Commands.CreateProductCommand;
+
+public class CreateProductResult
+{
+    public Guid Id { get; init; }
+    public string? Name { get; init; }
+    public string? Description { get; init; }
+    public decimal Price { get; init; }
+    public string? CreatedBy { get; init; }
+}
