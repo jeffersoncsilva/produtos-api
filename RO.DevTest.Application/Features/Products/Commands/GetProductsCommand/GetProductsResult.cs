5eab4bfd3d34fedc3139571bedc76a1c3c5e63c4
Jefferson C. Silva <jefferson.c.silva@outlook.com>
2025-04-25 10:33:09 -0300

    🚧adicionado comandos para recuperar produtos por paginação.

---
+++ RO.DevTest.Application/Features/Products/Commands/GetProductsCommand/GetProductsResult.cs
@@ -0,0 +1,8 @@
+﻿namespace RO.DevTest.Application.Features.Products.Commands.GetProductsCommand;
+
+public class GetProductsResult
+{
+    public IReadOnlyList<RO.DevTest.Domain.Entities.Product>? Products { get; init; }
+    public int Page { get; init; }
+    public int Size { get; init; }
+}
