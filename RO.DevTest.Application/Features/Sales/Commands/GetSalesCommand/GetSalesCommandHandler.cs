94a50b01782b3addbaa182267a8ae2a5ad278b3f
Jefferson C. Silva <jefferson.c.silva@outlook.com>
2025-04-25 13:55:54 -0300

    🚧adicionado comandos que recuperam uma venda.
    Adicinado comandos que fazem a pesquisa por uma venda de maneira paginada.

---
+++ RO.DevTest.Application/Features/Sales/Commands/GetSalesCommand/GetSalesCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using RO.DevTest.Application.Contracts.Persistance.Repositories;
+
+namespace RO.DevTest.Application.Features.Sales.Commands.GetSalesCommand;
+public class GetSalesCommandHandler(ISaleRepository saleRepository) : IRequestHandler<GetSalesCommandRequest, GetSalesResult>
+{
+	public async Task<GetSalesResult> Handle(GetSalesCommandRequest request, CancellationToken ct)
+	{
+		int page = request.Page;
+		int size = request.Size == 0 ? 10 : request.Size;
+
+		var sales = await saleRepository.GetPagedSalesAsync(page, size, ct);
+
+		return new GetSalesResult()
+		{
+			Page = page,
+			Size = sales.Count,
+			Sales = sales
+		};
+	}
+}
---
+++ RO.DevTest.Application/Features/Sales/Commands/GetSalesCommand/GetSalesResult.cs
@@ -0,0 +1,9 @@
+﻿using RO.DevTest.Domain.Entities;
+
+namespace RO.DevTest.Application.Features.Sales.Commands.GetSalesCommand;
+public class GetSalesResult
+{
+	public IReadOnlyList<Sale> Sales { get; set; } = [];
+	public int Page { get; set; }
+	public int Size { get; set; }
+}
