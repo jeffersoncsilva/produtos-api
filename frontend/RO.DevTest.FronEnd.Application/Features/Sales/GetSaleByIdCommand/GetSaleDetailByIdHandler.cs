dd56fbcba3aab492e7c6f5aa7898b476d8694cfd
Jefferson C. Silva <jefferson.c.silva@outlook.com>
2025-04-26 19:42:17 -0300

    🚧 criado handlers para obter dados sobre as vendas e criar uma venda

---
+++ frontend/RO.DevTest.FronEnd.Application/Features/Sales/GetSaleByIdCommand/GetSaleDetailByIdHandler.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using RO.DevTest.FrontEnd.ViewModels.Exceptions;
+
+namespace RO.DevTest.FronEnd.Application.Features.Sales.GetSaleByIdCommand;
+
+public class GetSaleDetailByIdHandler(IHttpClientFactory httpFactory) : IRequestHandler<GetSaleDetailByIdRequest, GetSaleDetailByIdResponse?>
+{
+	public async Task<GetSaleDetailByIdResponse?> Handle(GetSaleDetailByIdRequest request, CancellationToken ct)
+	{
+		try
+		{
+			var httpClient = httpFactory.CreateClient(HttpConfiguration.HttpClientName);
+
+			var result = await httpClient.GetAsync($"api/sales?id={request.Id}", ct);
+
+			result.EnsureSuccessStatusCode();
+
+			JsonSerializerOptions op = new()
+			{
+				ReferenceHandler = ReferenceHandler.Preserve
+			};
+
+			var obj = await JsonSerializer.DeserializeAsync<GetSaleDetailByIdResponse>(await result.Content.ReadAsStreamAsync(), op, cancellationToken: ct);
+			return obj;
+		}
+		catch (Exception ex)
+		{
+			throw new BadRequestException(System.Net.HttpStatusCode.InternalServerError, ex.Message);
+		}
+	}
+}
