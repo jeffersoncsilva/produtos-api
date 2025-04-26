653e371a6bd55549a8db356287df0f68a47faf49
Jefferson C. Silva <jefferson.c.silva@outlook.com>
2025-04-26 11:27:15 -0300

    💄 adicionado handler de obter todos os detalhes de um produto por id

---
+++ frontend/RO.DevTest.FronEnd.Application/Features/Products/GetProductByIdCommand/GetProductByIdCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using RO.DevTest.FronEnd.Application.Features.Products.CreateProductCommand;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using System.Text;
+using RO.DevTest.FrontEnd.ViewModels.Exceptions;
+
+namespace RO.DevTest.FronEnd.Application.Features.Products.GetProductByIdCommand;
+
+public class GetProductByIdCommandHandler(IHttpClientFactory httpFactory) : IRequestHandler<GetProductByIdCommandRequest, GetProductByIdResponse?>
+{
+	public async Task<GetProductByIdResponse?> Handle(GetProductByIdCommandRequest request, CancellationToken ct)
+	{
+		try
+		{
+			var httpClient = httpFactory.CreateClient(HttpConfiguration.HttpClientName);
+			var uri = $"api/products/{request.Id}";
+			var result = await httpClient.GetAsync(uri, ct);
+
+			result.EnsureSuccessStatusCode();
+
+			JsonSerializerOptions op = new()
+			{
+				ReferenceHandler = ReferenceHandler.Preserve
+			};
+
+			var obj = await JsonSerializer.DeserializeAsync<GetProductByIdResponse>(await result.Content.ReadAsStreamAsync(), op, cancellationToken: ct);
+			return obj;
+		}
+		catch(Exception ex)
+		{
+			throw new BadRequestException(System.Net.HttpStatusCode.InternalServerError, ex.Message);
+		}
+	}
+}
---
+++ frontend/RO.DevTest.FronEnd.Application/Features/Products/GetProductByIdCommand/GetProductByIdCommandRequest.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace RO.DevTest.FronEnd.Application.Features.Products.GetProductByIdCommand;
+
+public sealed class GetProductByIdCommandRequest(Guid? id) : IRequest<GetProductByIdResponse>
+{
+	public Guid Id { get; init; } = id ?? Guid.Empty;
+}
