80ba8f04f097e946ee0cede2a1935835947fe18c
Jefferson C. Silva <jefferson.c.silva@outlook.com>
2025-04-26 11:25:44 -0300

    ♻️ adicionado campos [JsonPropertName] e adiçao de proriedade de criado em e modificado em

--- RO.DevTest.Application/Features/Products/Commands/GetProductByIdCommand/GetProductByIdResult.cs
+++ RO.DevTest.Application/Features/Products/Commands/GetProductByIdCommand/GetProductByIdResult.cs
@@ -1,18 +1,45 @@
-﻿namespace RO.DevTest.Application.Features.Products.Commands.GetProductByIdCommand;
+﻿using System.Text.Json.Serialization;
+
+namespace RO.DevTest.Application.Features.Products.Commands.GetProductByIdCommand;
 
 public class GetProductByIdResult
 {
+    [JsonPropertyName("id")]
     public Guid Id { get; init; }
+    
+    [JsonPropertyName("created_on")]
     public DateTime CreatedOn { get; init; }
-    public DateTime ModifiedOn { get; init; }
+	
+    [JsonPropertyName("modified_on")]
+	public DateTime ModifiedOn { get; init; }
+    
+    [JsonPropertyName("name")]
     public string? Name { get; init; }
+    
+    [JsonPropertyName("description")]
     public string? Description { get; init; }
+    
+    [JsonPropertyName("price")]
     public decimal Price { get; init; }
+    
+    [JsonPropertyName("image_url")]
     public string? ImageUrl { get; init; }
+
+    [JsonPropertyName("category")]
     public string? Category { get; init; }
+
+    [JsonPropertyName("brand")]
     public string? Brand { get; init; }
+
+    [JsonPropertyName("stock")]
     public int Stock { get; init; }
+
+    [JsonPropertyName("is_active")]
     public bool IsActive { get; init; }
+
+    [JsonPropertyName("created_by")]
     public string? CreatedBy { get; init; }
-    public string? ModifiedBy { get; init; }
+
+	[JsonPropertyName("modified_by")]
+	public string? ModifiedBy { get; init; }
 }
