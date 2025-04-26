953a3be5962c8fcff821ba6961fb0e37a2416f2f
Jefferson C. Silva <jefferson.c.silva@outlook.com>
2025-04-26 11:28:37 -0300

    ✨ adicinado pagina e handlers para ediçao de um produto.

---
+++ frontend/RO.DevTest.FronEnd.Application/Features/Products/UpdateProductCommand/UpdateProductCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using RO.DevTest.FronEnd.Application.Features.Products.CreateProductCommand;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using System.Text;
+using RO.DevTest.FrontEnd.ViewModels.Exceptions;
+
+namespace RO.DevTest.FronEnd.Application.Features.Products.UpdateProductCommand;
+
+public class UpdateProductCommandHandler(IHttpClientFactory httpFactory) : IRequestHandler<UpdateProductCommandRequest, UpdateProductCommandResponse?>
+{
+	public async Task<UpdateProductCommandResponse?> Handle(UpdateProductCommandRequest request, CancellationToken ct)
+	{
+		try
+		{
+			var httpClient = httpFactory.CreateClient(HttpConfiguration.HttpClientName);
+
+			using StringContent jsonContent = new(JsonSerializer.Serialize(request), Encoding.UTF8, "application/json");
+
+			var result = await httpClient.PutAsync("api/products", jsonContent, ct);
+
+			result.EnsureSuccessStatusCode();
+
+			JsonSerializerOptions op = new()
+			{
+				ReferenceHandler = ReferenceHandler.Preserve
+			};
+
+			var obj = await JsonSerializer.DeserializeAsync<UpdateProductCommandResponse>(await result.Content.ReadAsStreamAsync(), op, cancellationToken: ct);
+			return obj;
+		}
+		catch (Exception ex)
+		{
+			throw new BadRequestException(System.Net.HttpStatusCode.InternalServerError, ex.Message);
+		}
+	}
+}
